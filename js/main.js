3096642892e3a7b8468b3a1c54b941f0b957420b
2skyyy <128768622+2skyyy@users.noreply.github.com>
2023-04-07 17:42:48 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -27,3 +27,23 @@ navBar.onclick = () => {
 closed.onclick = () => {
    sideWrap.classList.remove('active')
 }
+
+let targetMainMenu = $("a.nav-link");
+
+targetMainMenu.click(function () {
+   var targetSubMenu = $(this).next("ul.nav-item");
+   for (var i = 0; i < targetMainMenu.length; i++) {
+      if ($(this).text() == targetMainMenu[i].innerText) {
+
+         if (targetSubMenu.is(":visible")) {
+            targetSubMenu.slideUp();
+         } else {
+            targetSubMenu.slideDown();
+         }
+      } else {
+         $(targetMainMenu[i]).next("ul.nav-item").css("display", "none");
+      }
+   }
+});
+
+
